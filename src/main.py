ddf1bdcf0d3d39a16ac1a76d72e0eb01761d133f
Starz0r <starz0r@starz0r.com>
2022-07-22 16:11:25 -0500

    configure yt-dlp format defaults

--- src/main.py
+++ src/main.py
@@ -36,6 +36,10 @@ ytdl_format_options = {
     "http_headers": {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
     },
+    "prefer_ffmpeg": True,
+    "dynamic_mpd": True,
+    "extractor_retries": 10,
+    "noprogress": True,
 }
 
 ffmpeg_options = {"options": "-vn"}
